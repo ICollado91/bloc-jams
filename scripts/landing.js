11a9ff7dbac6eb30500d0505961db436e3b08ff3
ICollado91 <collado.ip@gmail.com>
2016-09-25 16:35:55 -0400

    add checkpoint events

--- scripts/landing.js
+++ scripts/landing.js
@@ -18,12 +18,12 @@ var animatePoints = function(points) {
 }
 
 window.onload = function () {
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
